4ce69dca6ea959675814ae9ec27aed2cf7e1b393
Joe Futrelle <joefutrelle@gmail.com>
2015-05-21 11:00:10 -0400

    now uses proper SRS for map and data, so the projection is correct in the output

--- make_raster.py
+++ make_raster.py
@@ -1,7 +1,6 @@
 import mapnik
-m = mapnik.Map(1200,1200)
-m.srs = "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +units=m +k=1.0 +nadgrids=@null +no_defs"
-
+m = mapnik.Map(2400,2400)
+m.srs = "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +units=m +k=1.0 +nadgrids=@null +no_defs +over"
 m.background = mapnik.Color('rgba(0,0,0,0)')
 s = mapnik.Style()
 r = mapnik.Rule()
@@ -22,19 +21,9 @@ layer.srs = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'
 layer.datasource = ds
 layer.styles.append('My Style')
 m.layers.append(layer)
-
-
-# http://svn.openstreetmap.org/applications/rendering/mapnik/generate_image.py
-# shows reprojection, but I don't have it working yet
-#longlat = mapnik.Projection('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
-#merc = mapnik.Projection('+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over')
-#transform = mapnik.ProjTransform(longlat,merc)
-#merc_box = transform.forward(mapnik.Box2d(-179,-89,179,89))
-#m.zoom_to_box(merc_box)
-
-#worldExtent = mapnik.Box2d(-180.0, -90.0, 180.0, 90.0)
+# worldExtent is in the map srs which is mercator
 worldExtent = mapnik.Box2d(-20037508.34,-20037508.34,20037508.34,20037508.34)
-m.zoom_to_box(worldExtent) # but we need to reproject to mercator
+m.zoom_to_box(worldExtent)
 OUTF='static/world.png'
 mapnik.render_to_file(m,OUTF,'png')
 print "rendered image to '%s'" % OUTF
